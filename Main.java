60a6288f052240c5aba0f3cc321b840d8112fde2
Braxton Hardman <braxtonhardman@gmail.com>
2026-04-23 19:01:21 -0700

    Massive changes introduced multi-threading

--- Main.java
+++ Main.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.lang.Thread; 
 import java.util.ArrayList;
 
 class Main { 
 
     public static Scanner scanner = null; 
-    public static PowerGrid grid = new PowerGrid(6, 6);
+    public static PowerGrid grid = new PowerGrid(20, 20);
     public static int numThreads = 4; 
+    public static PowerGrid[][] thread_objects = null;
     public static ArrayList<Thread> threads = new ArrayList<>(); 
 
     public static void main(String[] args) {
@@ -20,16 +20,16 @@ class Main {
             
             optionLoop(); 
         
-            for(int i = 0; i < grid.getCols(); i++) { 
-                for(int j = 0; j < grid.getRows(); j++){ 
-                    if(grid.hasLight(i, j)) { 
-                        System.out.print("X ");
-                    } else { 
-                        System.out.print("- ");
-                    }
-                }
-                System.out.println("");
-            }
+            // for(int i = 0; i < grid.getCols(); i++) { 
+            //     for(int j = 0; j < grid.getRows(); j++){ 
+            //         if(grid.hasLight(i, j)) { 
+            //             System.out.print("X ");
+            //         } else { 
+            //             System.out.print("- ");
+            //         }
+            //     }
+            //     System.out.println("");
+            // }
 
             scanner.close();
         } catch(Exception e) {  
@@ -62,7 +62,7 @@ class Main {
                         "2. Add light\n" + //
                         "3. Set x/y size (default is 6/6)\n" + //
                         "4. Run the program\n" + //
-                        "5. Enter the number of threads (default is 4)");
+                        "5. Enter the number of threads (default is 4)\n");
 
             input = scanner.nextLine();
 
@@ -115,32 +115,152 @@ class Main {
                         break;
                         case 4:
 
+                            
                             // Divide into subgrids for each thread. 
-                            int threadRows = 2; 
-                            int threadCols = numThreads / 2;
+                            System.out.println("----Main Subgrid Seperation----");
+                            System.out.println("Thread Cols: " + numThreads);
 
                             // width of each chunk 
-                            int chunkWidth = grid.getCols() / threadCols; 
-                            int chunkHeight = grid.getRows() / threadRows; 
+                            int chunkWidth = grid.getCols() / numThreads; 
+                            int chunkHeight = grid.getRows() / numThreads; 
 
-                            for(int row = 0; row < threadRows; row++) {
+                            int extraWidth = grid.getCols() % numThreads; 
+                            int extraHeight = grid.getRows() % numThreads; 
+
+                            int threadCols = numThreads / 2; 
+
+                            thread_objects = new PowerGrid[2][threadCols];
+
+                            // Initalizes subgrids for each thread storing in threads arraylist 
+                            for(int row = 0; row < 2; row++) {
                                 for(int col = 0; col < threadCols; col++) {
                                     int startX = col * chunkWidth;
-                                    int startY = row * chunkHeight;
+                                    int startY = row * chunkWidth;
 
-                                    System.out.println(startX + " " + startY);
-                                    PowerGrid p = new PowerGrid(grid, startX, startY, chunkWidth, chunkHeight);
-                                    Thread thread_obj = new Thread(p);
+                                    // For odd number grids append to the last grid of each row 
+                                    // System.out.println("Grid: " + row + " " + col);
+                                    PowerGrid p = null;
+                                    if(row == 1) { 
+                                        if(extraHeight != 0  && col == (threadCols - 1)) { 
+                                            p = new PowerGrid(grid, startX, startY, chunkWidth + extraWidth, chunkHeight + extraHeight);
+                                        } else if (extraHeight != 0) { 
+                                            p = new PowerGrid(grid, startX, startY, chunkWidth, chunkHeight + extraHeight); 
+                                        } else { 
+                                            p = new PowerGrid(grid, startX, startY, chunkWidth, chunkHeight); 
+                                        }
+                                    } else { 
+                                        // row = 0 -> can only be the last end extend width
+                                        if(extraWidth != 0 && col == (threadCols - 1)) { 
+                                            p = new PowerGrid(grid, startX, startY, chunkWidth + extraWidth, chunkHeight);
+                                        } else { 
+                                            p = new PowerGrid(grid, startX, startY, chunkWidth, chunkHeight);
+                                        }
+                                    }
+                                    
+                                    if(p != null) { 
+                                        thread_objects[row][col] = p;
+                                    } else { 
+                                        System.out.println("Error: Thread was undefined when creating subgrids");
+                                        System.exit(0);
+                                    }
 
-                                    // Copy directly while creating the thread
-                                    threads.add(thread_obj);
+                                    
+                                }
+                            }   
+
+                            /*
+                                Set up communication between objects
+                                is in nice 2d array so know the position of each object
+                            */ 
+                            for(int row = 0; row < 2; row++) { 
+                                for(int col = 0; col < threadCols; col++) { 
+                                    // For each position check left right top bottom 
+                                    PowerGrid left, right, top, bottom; 
+                                    // Top bottom check 
+                                    if(row == 0) { 
+                                        top = null; 
+                                        bottom = thread_objects[row + 1][col];  
+                                    } else { 
+                                        bottom = null;
+                                        top = thread_objects[row - 1][col];
+                                    }
+
+                                    // Left Right Check 
+                                    if(col == 0) { 
+                                        left = null;
+                                        right = thread_objects[row][col +1];
+                                    } else if(col == threadCols -1) { 
+                                        right = null; 
+                                        left = thread_objects[row][col - 1];
+                                    } else { 
+                                        // Has all 3 
+                                        left = thread_objects[row][col - 1];
+                                        right = thread_objects[row][col + 1]; 
+                                    }
+
+
+                                    PowerGrid p = thread_objects[row][col];
+                                    p.setComm(left, right, top, bottom);
+                                    
                                 }
                             }
 
+                            /*
+                                Add Threads to an arrayList
+                            */
+                            for(int row = 0; row < 2; row++) { 
+                                for(int col = 0; col < threadCols; col++) { 
+                                    Thread temp = new Thread(thread_objects[row][col]);
+                                    threads.add(temp); 
+                                }
+                            }
+
+                            /*
+                                Start all threads
+                            */
+                            System.out.println("Starting threads...");
+                            for(Thread t : threads) {
+                                t.start();
+                            }
+
+                            /*
+                                Wait tell all threads are finished 
+                            */
+                            for(Thread t : threads) {
+                                try { 
+                                    t.join();
+                                } catch (Exception e) { 
+                                    e.printStackTrace();
+                                    System.exit(0);
+                                }
+                            }
+
+                            /*
+                                Combine all grids backinto one and print
+                            */  
+                            for(int row = 0; row < 2; row++) {
+                                for(int col = 0; col < threadCols; col++) {
+                                    PowerGrid chunk = thread_objects[row][col];
+                                    int startX = chunk.getStartX();
+                                    int startY = chunk.getStartY();
+
+                                    for(int i = 0; i < chunk.getCols(); i++) {
+                                        for(int j = 0; j < chunk.getRows(); j++) {
+                                            grid.setPower(startX + i, startY + j, chunk.getPower(i, j));
+                                            if(chunk.hasLight(i, j)) {
+                                                grid.setLight(startX + i, startY + j);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+
+                            grid.outputCurrentGrid();
                             break; 
                         case 5: 
                             System.out.println("Enter the number of threads: ");
                             String line = scanner.nextLine();
+                            // Recall resizing mechanism 
 
                     default: 
                         System.out.println("Error: Please select a value between 1 and 4");
--- PowerGrid.java
+++ PowerGrid.java
@@ -4,8 +4,13 @@ public class PowerGrid implements Runnable {
     private int[][] power_grid;
     private char[][] light_grid;
     private boolean[][] source_grid; 
-    private int rows;
-    private int cols;
+    private int rows, cols, totalRows, totalCols, startX, startY; 
+    private boolean left, right, top, bottom; 
+
+    private int[] ghost_left;    // values from left neighbor's right column
+    private int[] ghost_right;   // values from right neighbor's left column
+    private int[] ghost_top;     // values from top neighbor's bottom row
+    private int[] ghost_bottom;  // values from bottom neighbor's top row
 
     private LinkedBlockingQueue<Integer> recv_left = null;
     private LinkedBlockingQueue<Integer> recv_right = null;
@@ -23,71 +28,233 @@ public class PowerGrid implements Runnable {
         this.source_grid = new boolean[rows][cols];
     }
 
+
+    public void setComm(PowerGrid left, PowerGrid right, PowerGrid top, PowerGrid bottom) { 
+        // Set all the sends to recievs from other power grids.
+        if(left != null) { 
+            this.left = true; 
+            this.recv_left = left.getSendRight(); 
+        }
+        if(right != null) { 
+            this.right = true; 
+            this.recv_right = right.getSendLeft(); 
+        }
+        if(top != null) { 
+            this.top = true; 
+            this.recv_vert = top.getSendVert();
+        }
+        if(bottom != null) { 
+            this.bottom = true; 
+            this.recv_vert = bottom.getSendVert();
+        }
+
+        System.out.println("----SetupComm----");
+        System.out.println("Send Left: " + this.recv_left + " Send Right: " + this.recv_right + " Send Vert: " + this.recv_vert);
+
+    }
+
+    private void write() throws InterruptedException { 
+        if(left) { 
+            // System.out.println("Writing Left..."); 
+
+            // Write first column (x=0) to send_left
+            for(int j = 0; j < rows; j++) { 
+                // System.out.print(" " + power_grid[0][j]);
+                send_left.put(power_grid[0][j]);
+            }
+        }
+    
+        if(right) { 
+            // System.out.println("Writing Right..."); 
+
+            // Write last column (x=cols-1) to send_right
+            for(int j = 0; j < rows; j++) { 
+                // System.out.print(" " + power_grid[cols - 1][j]);
+                send_right.put(power_grid[cols - 1][j]);
+            }
+        }
+    
+        if(top) { 
+            // System.out.println("Writing Top..."); 
+
+            // Write first row (y=0) to send_top
+            for(int i = 0; i < cols; i++) { 
+                // System.out.print(" " + power_grid[i][0]);
+                send_vert.put(power_grid[i][0]);
+            }
+        }
+    
+        if(bottom) { 
+            // System.out.println("Writing Bottom..."); 
+
+            // Write last row (y=rows-1) to send_bottom
+            for(int i = 0; i < cols; i++) { 
+                // System.out.print(" " + power_grid[i][rows - 1]);
+                send_vert.put(power_grid[i][rows - 1]);
+            }
+        }
+    }
+
+    private void read() throws InterruptedException { 
+        if(left) {
+            for(int j = 0; j < rows; j++) { 
+                    
+                ghost_left[j] = recv_left.take();
+            }
+        }
+        if(right) { 
+
+            for(int j = 0; j < rows; j++) { 
+                ghost_right[j] = recv_right.take();
+            }
+        }
+        if(top) { 
+
+            for(int i = 0; i < cols; i++) { 
+                ghost_top[i] = recv_vert.take();
+            }
+        }
+        if(bottom) { 
+
+            for(int i = 0; i < cols; i++) { 
+                ghost_bottom[i] = recv_vert.take();
+            }
+        }
+    }
+
+    public LinkedBlockingQueue<Integer> getSendLeft() { 
+        return this.send_left; 
+    }
+
+    public LinkedBlockingQueue<Integer> getSendRight() { 
+        return this.send_right; 
+
+    }
+
+    public LinkedBlockingQueue<Integer> getSendVert() { 
+        return this.send_vert; 
+
+    }
+
+    public LinkedBlockingQueue<Integer> getRecvLeft() { 
+        return this.recv_left; 
+    }
+
+    public LinkedBlockingQueue<Integer> getRecvRight() { 
+        return this.recv_right;
+    }
+
+    public LinkedBlockingQueue<Integer> getRecvVert() { 
+        return this.recv_vert;
+    }
+
     /*
         Constructor is for creation of threads 
     */
     public PowerGrid(PowerGrid grid, int startX, int startY, int width, int height) { 
-        int x = grid.getCols() - startX; 
-        int y = grid.getRows() - startY; 
-        int[][] new_power_grid = new int[x][y]; 
-        char[][] new_light_grid = new char[x][y];
-        boolean[][] new_source_grid = new boolean[x][y];
-        for(int i = startX; i < grid.getCols(); i++) { 
-            for(int j = startY; j < grid.getRows(); j++) { 
-                new_power_grid[i][j] = grid.getPower(i, j);
-                new_light_grid[i][j] = grid.getLight(i, j);
-                new_source_grid[i][j] = grid.getSourcePower(i, j);
+
+        this.totalRows = grid.getRows(); 
+        this.totalCols = grid.getCols(); 
+
+        this.startX = startX;
+        this.startY = startY; 
+
+        this.send_left = new LinkedBlockingQueue<>();
+        this.send_right = new LinkedBlockingQueue<>();
+        this.send_vert = new LinkedBlockingQueue<>();
+     
+
+        // System.out.println("----Power Grid Output----");
+        // System.out.println("Old Grid Size: " + grid.getCols() + "x" + grid.getRows());
+        // System.out.println("New Grid Size: " + width + "x" + height);
+        // System.out.println("StartX: " + startX + " StartY: " + startY); 
+        this.rows = height;
+        this.cols = width;
+
+        this.ghost_left   = new int[rows];
+        this.ghost_right  = new int[rows];
+        this.ghost_top    = new int[cols];
+        this.ghost_bottom = new int[cols];
+        // System.out.println("Cols: " + this.cols + " Rows: " + this.rows);
+        int[][] new_power_grid      = new int[this.rows][this.cols]; 
+        char[][] new_light_grid     = new char[this.rows][this.cols];
+        boolean[][] new_source_grid = new boolean[this.rows][this.cols];
+
+        for(int i = 0; i < width; i++) { 
+            for(int j = 0; j < height; j++) {
+                new_power_grid[j][i]   = grid.getPower(startX + i, startY + j);
+                new_light_grid[j][i]   = grid.getLight(startX + i, startY + j);
+                new_source_grid[j][i]  = grid.getSourcePower(startX + i, startY + j);
             }
         }
 
-        this.power_grid = new_power_grid; 
-        this.light_grid = new_light_grid; 
-        this.source_grid = new_source_grid; 
+        this.power_grid  = new_power_grid; 
+        this.light_grid  = new_light_grid; 
+        this.source_grid = new_source_grid;
+
+        // outputCurrentGrid();
     }   
 
     @Override 
     public void run() {     
 
-        boolean changed = true;
-        while(changed) {
-            changed = false;
-            for(int i = 0; i < getCols(); i++) {
-                for(int j = 0; j < getRows(); j++) {
-                    if(!getSourcePower(i, j)) {
-                        int left   = (i - 1 >= 0)         ? getPower(i-1, j) : 0;
-                        int right  = (i + 1 < getCols())  ? getPower(i+1, j) : 0;
-                        int top    = (j - 1 >= 0)         ? getPower(i, j-1) : 0;
-                        int bottom = (j + 1 < getRows())  ? getPower(i, j+1) : 0;
-    
-                        int max = Math.max(Math.max(left, right), Math.max(top, bottom));
-                        int newPower = Math.max(0, max - 1); // floor at 0
-                        
-                        // Check if position is light
-                        if(hasLight(i, j)) { 
-                            if(newPower > 5) { 
-                                setLight(i, j);
+        // Exchange information first 
+        try { 
+            write(); 
+            read(); 
+        } catch (Exception e) { 
+            e.printStackTrace();
+        }
+
+        int totalRuns = totalRows * totalCols; 
+        for(int run = 0; run < totalRuns; run++) { 
+            boolean changed = true;
+            while(changed) {
+                changed = false;
+                for(int i = 0; i < getCols(); i++) {
+                    for(int j = 0; j < getRows(); j++) {
+                        if(!getSourcePower(i, j)) {
+                            int left   = (i - 1 >= 0)         ? getPower(i-1, j) : 0;
+                            int right  = (i + 1 < getCols())  ? getPower(i+1, j) : 0;
+                            int top    = (j - 1 >= 0)         ? getPower(i, j-1) : 0;
+                            int bottom = (j + 1 < getRows())  ? getPower(i, j+1) : 0;
+        
+                            int max = Math.max(Math.max(left, right), Math.max(top, bottom));
+                            int newPower = Math.max(0, max - 1); // floor at 0
+                            
+                            // Check if position is light
+                            if(hasLight(i, j)) { 
+                                if(newPower > 5) { 
+                                    setLight(i, j);
+                                }
                             }
-                        }
 
-                        if(newPower != getPower(i, j)) {
-                            setPower(i, j, newPower);
-                            changed = true;
-                        }
+                            if(newPower != getPower(i, j)) {
+                                setPower(i, j, newPower);
+                                changed = true;
+                            }
 
-                        
+                            
+                        }
                     }
                 }
             }
-        }
+        }   
+        
 
-
-        outputCurrentGrid(); 
     }   
 
+    public int getStartX() { 
+        return this.startX;
+    }
+
+    public int getStartY() { 
+        return this.startY; 
+    }
+
     public void outputCurrentGrid() { 
-        // Outputs the current grid 
-        for(int i = 0; i < getCols(); i++) { 
-            for(int j = 0; j < getRows(); j++){ 
+        for(int j = 0; j < getRows(); j++) {        // y is outer loop
+            for(int i = 0; i < getCols(); i++) {    // x is inner loop
                 if(hasLight(i, j)) { 
                     System.out.print("X ");
                 } else { 
@@ -97,6 +264,7 @@ public class PowerGrid implements Runnable {
             System.out.println("");
         }
     }
+
     public void resize(int newRows, int newCols) {
         int[][] new_power_grid = new int[newRows][newCols];
         char[][] new_light_grid = new char[newRows][newCols];
@@ -117,31 +285,31 @@ public class PowerGrid implements Runnable {
     }
 
     public void setPower(int x, int y, int power) {
-        power_grid[x][y] = power;
+        power_grid[y][x] = power;
     }
 
     public int getPower(int x, int y) {
-        return power_grid[x][y];
+        return power_grid[y][x];
     }
 
     public void setSourcePower(int x, int y) { 
-        source_grid[x][y] = true; 
+        source_grid[y][x] = true; 
     }
 
     public boolean getSourcePower(int x, int y) { 
-        return this.source_grid[x][y]; 
+        return this.source_grid[y][x]; 
     }
 
     public char getLight(int x, int y) { 
-        return this.light_grid[x][y]; 
+        return this.light_grid[y][x]; 
     }
 
     public void setLight(int x, int y) {
-        light_grid[x][y] = 'X'; 
+        light_grid[y][x] = 'X'; 
     }
 
     public boolean hasLight(int x, int y) {
-        return light_grid[x][y] == 'X';
+        return light_grid[y][x] == 'X';
     }
 
     public int[][] getPowerGrid() { 
